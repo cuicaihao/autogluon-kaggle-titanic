1d82732fa6f32eb701ba603013524889eb5360bf
Chris (Caihao) Cui <cuicaihao@users.noreply.github.com>
2021-08-19 12:38:01 +1000

    rebuilt model with best_quality feature

--- run.py
+++ run.py
@@ -3,16 +3,21 @@ from autogluon.tabular import TabularDataset, TabularPredictor
 train_data = TabularDataset('train.csv')
 id, label = 'PassengerId', 'Survived'
 save_path = 'model'
+
+time_limit = 300
+
 predictor = TabularPredictor(label=label,
-                             path=save_path).fit(train_data.drop(columns=[id]))
+                             path=save_path).fit(train_data.drop(columns=[id]),
+                                                 time_limit=time_limit,
+                                                 presets='best_quality')
 # Test
 import pandas as pd
 
 test_data = TabularDataset('test.csv')
 
-predictor = TabularPredictor.load(
-    save_path
-)  # unnecessary, just demonstrates how to load previously-trained predictor from file
+# predictor = TabularPredictor.load(
+#     save_path
+# )  # unnecessary, just demonstrates how to load previously-trained predictor from file
 
 preds = predictor.predict(test_data.drop(columns=[id]))
 submission = pd.DataFrame({id: test_data[id], label: preds})
